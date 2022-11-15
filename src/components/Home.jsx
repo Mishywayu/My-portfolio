1486ed68814fc7e7850fb2b3f0878da2367e3100
Mishywayu <michellewayua2@gmail.com>
2022-11-15 12:57:18 +0300

    My portfolio

---
+++ src/components/Home.jsx
@@ -0,0 +1,24 @@
+// import React from "react";
+import '../App.css';
+import Navbar from './Navbar';
+
+export default function Home() {
+    return(
+        <div className='home-container'>
+            <h2>Hello, I'm <span className='special-text'>Michelle Wayua</span>. <br />
+            I'm a frontend developer.</h2>
+            <div className='box'>
+               <div></div>
+               <div></div>
+               <div></div>
+               <div></div>
+               <div></div>
+               <div></div>
+               <div></div>
+               <div></div>
+               <div></div>
+               <div></div>
+            </div>
+        </div>
+    );
+}
